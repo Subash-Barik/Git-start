d59e21005e65c764572540ea7743001c399b95f9
Subash-Barik <sbbarik143@gmail.com>
2023-11-10 00:49:50 +0530

    listform

--- demo.js
+++ demo.js
@@ -1,1 +1,14 @@
-console.log(welcome);
+console.log("subash barik");
+console.log(document.URL);
+console.log(document.title);
+var header=document.getElementById("header-title");
+
+console.log(header);
+header.style.borderBottom="solid 3px black";
+
+
+
+var legend=document.getElementById('legend');
+console.log(legend);
+legend.style.fontWeight='bold';
+legend.style.color='green';
